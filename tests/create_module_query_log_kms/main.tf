a1e152f5ad243bd05e90a528cf9a309a550839bc
Loren Gordon <lorengordon@users.noreply.github.com>
2020-06-26 07:25:57 -0700

    Rewrites test for route53 query log with kms encryption

---
+++ tests/create_module_query_log_kms/main.tf
@@ -0,0 +1,39 @@
+provider aws {
+  region = "us-east-1"
+}
+
+module "query_log" {
+  source = "../..//modules/query-log"
+
+  providers = {
+    aws = aws
+  }
+
+  create_route53_query_log = true
+
+  query_log_bucket         = data.terraform_remote_state.prereq.outputs.bucket["bucket"].id
+  query_log_bucket_kms_key = data.terraform_remote_state.prereq.outputs.kms.keys[data.terraform_remote_state.prereq.outputs.id].arn
+  zone_id                  = aws_route53_zone.this.zone_id
+
+  tags = {
+    environment = "testing"
+  }
+}
+
+resource "aws_route53_zone" "this" {
+  name = "${random_string.id.result}.com"
+}
+
+resource "random_string" "id" {
+  length  = 8
+  upper   = false
+  special = false
+  number  = false
+}
+
+data "terraform_remote_state" "prereq" {
+  backend = "local"
+  config = {
+    path = "prereq/terraform.tfstate"
+  }
+}
